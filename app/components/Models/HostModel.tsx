88bc7d755576690461286ff9dc15f4bb09b721de
Pranay Bolem <pranaybolem@gmail.com>
2024-10-01 15:17:57 -0230

    Added the introduction, basics pages and counter logic for user while adding guests and rooms

--- app/components/Models/HostModel.tsx
+++ app/components/Models/HostModel.tsx
@@ -8,19 +8,22 @@ import CategoryInput from "../Inputs/CategoryInput";
 import CountrySelect from "../Inputs/CountrySelect";
 import { FieldValue, FieldValues, useForm } from "react-hook-form";
 import dynamic from "next/dynamic";
+import Counter from "../Inputs/Counter";
 
 enum STEPS {
-    CATEGORY    = 0,
-    LOCATION    = 1,
-    INFO        = 2,
-    IMAGES      = 3,
-    DESCRIPTION = 4,
-    PRICE       = 5,
+    INTRODUCTION= 0,
+    BASICS      = 1,
+    CATEGORY    = 2,
+    LOCATION    = 3,
+    INFO        = 4,
+    IMAGES      = 5,
+    DESCRIPTION = 6,
+    PRICE       = 7,
 }
 
 const HostModel = () => {
     const HostModel = useHostModel();
-    const [step, setStep] = useState(STEPS.CATEGORY);
+    const [step, setStep] = useState(STEPS.INTRODUCTION);
 
     const {
         register,
@@ -47,6 +50,9 @@ const HostModel = () => {
 
     const category = watch('category');
     const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
         ssr: false
@@ -77,7 +83,7 @@ const HostModel = () => {
     }, [step]);
 
     const secondaryActionLabel = useMemo (() => {
-        if (step === STEPS.CATEGORY) {
+        if (step === STEPS.INTRODUCTION) {
             return undefined;
         }
 
@@ -86,30 +92,109 @@ const HostModel = () => {
 
     let bodyContent = (
         <div className="flex flex-col gap-8">
-            <Heading
-                title="Which of these best describes your place?"
-                subtitle="Pick a category"
-            />
-            <div className="
+                <h1 className="text-5xl">
+                    It's easy to get started
+                </h1>
+                <div className="
                 grid
-                grid-cols-1
-                md:grid-cols-2
                 gap-3
                 max-h-[50vh]
-                overflow-y-auto">
-                    {categories.map((item) => (
-                        <div key={item.label} className="col-span-1">
-                            <CategoryInput 
-                                onClick = {(category) => setCustomValue('category', category)} // defining id of the input and then value
-                                selected = {category === item.label}
-                                label = {item.label}
-                                icon = {item.icon}
+                overflow-auto">
+                    <div className="grid grid-cols-[1fr_0.5fr] gap-3 items-center">
+                        <div>
+                        <Heading
+                            title="1  Tell us about your place"
+                            subtitle="  Share some basic info, like where is is and how many guests can stay."
+                        />
+                        </div>
+                        <div className="flex items-center justify-normal ml-2">
+                            <img 
+                                src="/images/interior.jpg"
+                                alt="image of interior"
+                                className="max-w-full max-h-[120px] object-contain"
                             />
                         </div>
-                    ))}
-            </div>
+                    </div>
+                    <hr></hr>
+                    <div className="grid grid-cols-[1fr_0.5fr] gap-3 items-center">
+                        <div>
+                        <Heading
+                            title="2  Make it stand out"
+                            subtitle="Add 5 or more photos plus a title and description we'll help you out."
+                        />
+                        </div>
+                        <div className="flex items-center justify-normal ml-2">
+                            <img 
+                                src="/images/bench.jpg"
+                                alt="image of interior"
+                                className="max-w-full max-h-[120px] object-contain"
+                            />
+                        </div>
+                    </div>
+                    <hr></hr>
+                    <div className="grid grid-cols-[1fr_0.5fr] gap-3 items-center">
+                        <div>
+                        <Heading
+                            title="3  Finish up and publish"
+                            subtitle="Choose a starting price, verify a few details, then publish your listing."
+                        />
+                        </div>
+                        <div className="flex items-center justify-normal ml-2">
+                            <img 
+                                src="/images/door.jpg"
+                                alt="image of interior"
+                                className="max-w-full max-h-[120px] object-contain"
+                            />
+                        </div>
+                    </div>
+                </div>
         </div>
+
     );
+
+    if (step === STEPS.BASICS){
+        bodyContent = (
+            <div className="flex flex-col gap-3">
+                <Heading
+                    title=" Lets start with the basics! tell us about your place"
+                    subtitle="We'll ask you which type of property you have and if guests will
+                    book the entire place or just a room. Then let us know the location and how many guests can stay"
+                />
+                <div className="flex justify-center items-center max-h-[50vh]">
+                    <video className="w-[85%] h-auto" src="/images/basic.mp4" autoPlay muted playsInline />
+                </div>
+            </div>
+        )
+    }
+
+    if (step === STEPS.CATEGORY){
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Which of these best describes your place?"
+                    subtitle="Pick a category"
+                />
+                <div className="
+                    grid
+                    grid-cols-1
+                    md:grid-cols-2
+                    gap-3
+                    max-h-[40vh]
+                    overflow-y-auto">
+                        {categories.map((item) => (
+                            <div key={item.label} className="col-span-1">
+                                <CategoryInput 
+                                    onClick = {(category) => setCustomValue('category', category)} // defining id of the input and then value
+                                    selected = {category === item.label}
+                                    label = {item.label}
+                                    icon = {item.icon}
+                                />
+                            </div>
+                        ))}
+                </div>
+            </div>
+        )
+    }
 
     if (step === STEPS.LOCATION) {
         bodyContent = (
@@ -129,6 +214,41 @@ const HostModel = () => {
         )
     }
 
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading 
+                    title="Share some basics about your place"
+                    subtitle="What amenities do you have"
+                />
+                <Counter 
+                    title="Guests"
+                    subtitle="How many guests do you allow?"
+                    value={guestCount}
+                    onChange={(value) => setCustomValue('guestCount', value)}
+                />
+                <hr/>
+                <Counter 
+                    title="Rooms"
+                    subtitle="How many rooms do you have?"
+                    value={roomCount}
+                    onChange={(value) => setCustomValue('roomCount', value)}
+                />
+                <hr/>
+                <Counter 
+                    title="Guests"
+                    subtitle="How many bathrooms do you have?"
+                    value={bathroomCount}
+                    onChange={(value) => setCustomValue('bathroomCount', value)}
+                />
+            </div>
+        )
+    }
+
+    if (step === STEPS.IMAGES) {
+        
+    }
+
     return (
         <Model 
         isOpen= {HostModel.isOpen}
@@ -136,8 +256,8 @@ const HostModel = () => {
         onSubmit={onNext}
         actionLabel={actionLabel}
         secondaryActionLabel={secondaryActionLabel}
-        secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
-        title="Be a host!"
+        secondaryAction={step === STEPS.INTRODUCTION ? undefined : onBack}
+        title="Become a host!"
         body={bodyContent}
         />
     );
